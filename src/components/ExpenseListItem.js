0949261cb7f83b43c20fd7c0c8f78cf0b92b0403
Paramvir Singh <paramvir109@gmail.com>
2019-02-23 18:42:30 +0530

    Formatted createdAt and amount values

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 //dispatch method available from connect
 export const ExpenseListItem = ({description, createdAt, amount, id, dispatch}) => (
@@ -8,7 +10,11 @@ export const ExpenseListItem = ({description, createdAt, amount, id, dispatch}) 
     <NavLink activeClassName="is-active" to={"/edit/" + id}>
          <h3>{description}</h3>
     </NavLink>
-        <p>{amount} - {createdAt}</p>
+        <p>
+            Rs.{numeral(amount/100).format('0,0.00')} 
+            - 
+            {moment(createdAt).format('Do MMM YYYY')}
+        </p>
        
     </div>
 );
